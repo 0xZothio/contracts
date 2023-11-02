3d2413b5bc3432c363739ea2cd5e48dadfcfe1a1
srinivaszoth <130602539+srinivaszoth@users.noreply.github.com>
2023-11-02 19:34:10 +0530

    Update IWhitelistManager.sol

--- contracts/Interfaces/IWhitelistManager.sol
+++ contracts/Interfaces/IWhitelistManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.20;
+pragma solidity 0.8.18;
 
 interface IWhitelistManager {
     function isWhitelisted(address _address) external view returns (bool);
